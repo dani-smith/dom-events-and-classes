6448c53bb70aa09016fc433e7c7bf7491c4ca961
unknown <dcis.smith@gmail.com$git config --global user.email dcis.smith@gmail.com>
2018-07-03 16:19:58 +1200

    first edit on game - makeBlue

--- game.js
+++ game.js
@@ -12,6 +12,7 @@ function bindEventListeners (dots) {
     // BIND YOUR EVENT LISTENERS HERE
     // The first one is provided for you
     dots[i].addEventListener('contextmenu', makeGreen)
+    dots[i].addEventListener('click', makeBlue)
   }
 }
 
@@ -21,7 +22,11 @@ function makeGreen (evt) {
   updateCounts()
 }
 
-// CREATE FUNCTION makeBlue HERE
+function makeBlue (evt) {
+  evt.preventDefault()
+  evt.target.classList.toggle('blue')
+  updateCounts()
+}
 
 // CREATE FUNCTION hide HERE
 
